0abf3582858dfa7ef297c5ba83c563f81820638a
Andre Braga Reis <andrebragareis@gmail.com>
2014-05-04 12:18:56 +0100

    Using boost.propertytree to load SUMO XML data.

--- gissumo.cpp
+++ gissumo.cpp
@@ -1,25 +1,57 @@
-#include <iostream>
-#include <pqxx/pqxx>
-#include "tinyxml.h"
-#include "tinystr.h"
+#include "gissumo.h"
+
+#define XML_PATH "./fcdoutput.xml"
 
 using namespace std;
-using namespace pqxx;
-bool isLineOfSight(pqxx::connection &c, float x1, float y1, float x2, float y2);
+using namespace boost;
+using namespace boost::property_tree;
+
+const ptree& empty_ptree(){
+    static ptree t;
+    return t;
+}
 
 int main(int, char *argv[])
 {
-	/* step 1: parse SUMO logs
+	/* Init step 1: parse SUMO logs
 	 * This expects SUMO's floating car data (FCD) output method with geographic
 	 * coordinates (--fcd-output.geo=true)
 	 * 
 	 * The resulting file is XML, with syntax as follows:
+	 * <fcd-export>
+	 *  <timestep time="">
+	 *   <vehicle id="" x="" y="" ... />
 	 * 
-	 * To parse, we're using TinyXML++, or 'ticpp'.
+	 * To parse, we're using boost propertytrees, which run on top of RapidXML,
+	 * as ticpp kept spewing linker errors.
 	 */
 
-	// this opens the connection to postgres
+	cout << "DEBUG loading XML..." << flush;
+
+	ptree tree;
+	read_xml(XML_PATH, tree);
+	const ptree & formats = tree.get_child("fcd-export", empty_ptree());
+
+	cout << " done.\n";
+
+    BOOST_FOREACH(const ptree::value_type &f, formats){
+        string at = f.first + ".<xmlattr>";
+        const ptree & attributes = f.second.get_child("<xmlattr>", empty_ptree());
+        BOOST_FOREACH(const ptree::value_type &v, attributes){
+            cout << at << ' ' << v.first.data() << '=' << v.second.data() << '\n';
+        }
+    }
+
+
+	/* Init step 2: open a connection to PostgreSQL
+	 * A password can be added to this string.
+	 */
 	pqxx::connection conn("dbname=shapefiledb user=abreis");
+
+	/* Simulation starts here.
+	 * 
+	 *
+	 */
 
 	if(isLineOfSight(conn,-40910,166245,-40810,166230) > 0) cout << "NLOS\n"; else cout << "LOS\n";
 }
@@ -27,7 +59,7 @@ int main(int, char *argv[])
 
 bool isLineOfSight (pqxx::connection &c, float x1, float y1, float x2, float y2)
 {
-        pqxx::work txn(c);
+	pqxx::work txn(c);
 	
 	pqxx::result r = txn.exec(
 		"SELECT COUNT(id) "
@@ -35,8 +67,6 @@ bool isLineOfSight (pqxx::connection &c, float x1, float y1, float x2, float y2)
 		"WHERE ST_Intersects(geom, ST_GeomFromText('LINESTRING(-40910 166245,-40810 166230)',27492))"
 	);
 	txn.commit();
-
-	cout << "test " << r[0][0].as<int>() << endl;
 
 	if(r[0][0].as<int>() > 0)
 		return 1;
